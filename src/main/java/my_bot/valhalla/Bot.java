f765dd030b6f1af00b3d8d14ab297919dd5d34b3
HDES <hlagierq1997@gmail.com>
2019-06-19 20:13:21 +0200

    Excepciones en el main Signed-off-by: HDES <hlagierq1997@gmail.com>

--- src/main/java/my_bot/valhalla/Bot.java
+++ src/main/java/my_bot/valhalla/Bot.java
@@ -1,13 +1,30 @@
 package my_bot.valhalla;
 
+import javax.security.auth.login.LoginException;
+
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 
 public class Bot 
 {
-    public static void main( String[] args ) throws Exception
+    public static void main( String[] args )
     {
+    	try 
+    	{  		
     	JDA api = new JDABuilder("NTI1MjczMTg5Mjg3MTk4NzIx.XQenDg.S9i-cUb3t65TApIIfcF2occW_Ls").addEventListener(new MyListener()).build().awaitReady();
+    	//el método awaitReady hace "Blocking" garantizando que el JDA se cargará guay
+
+    	} 
+    	
+    	catch (LoginException e) {
+    		// Esto avisará de error en caso de que haya fallo en el proceso de autentificación
+    		e.printStackTrace();
+    	}
+    	
+    	catch (InterruptedException e) {
+    		// Controla el método awaitReady()
+    		e.printStackTrace();
+    	}
     	
     }
     
--- src/main/java/my_bot/valhalla/MyListener.java
+++ src/main/java/my_bot/valhalla/MyListener.java
@@ -11,11 +11,12 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 public class MyListener extends ListenerAdapter {
 
-	final String prefijo = ";"; // Prefijo usado antes de los comandos
+	final String prefijo = "!!"; // Prefijo usado antes de los comandos
 	final int tamPrefijo = prefijo.length(); // Longitud de la cadena del prefijo
 	final String comando1 = prefijo.concat("ping");
 	final String comando2 = prefijo.concat("limpiar");
 	final String comando3 = prefijo.concat("borrar");
+	final String comando4 = prefijo.concat("prefix");
 	final String botClientID = "525273189287198721";
 
 	@Override
@@ -33,20 +34,44 @@ public class MyListener extends ListenerAdapter {
 			channel.sendMessage("Holi").queue(); // Important to call .queue() on the RestAction returned by sendMessage(...)
 
 		} else if (content.equals(comando2)) {
-			TextChannel channel = event.getTextChannel();
-			int i = 10;
-			List <Message> messageList = new ArrayList();
-			for (Message message2 : channel.getIterableHistory()) {// substring lee desde la posicion 0, 1 carácter
-				if (message2.getAuthor().getId().equals(botClientID) || message2.getContentRaw().substring(0, tamPrefijo).equals(prefijo)) {
-			         messageList.add(message2);
-				} 
-
-				if (i-- < 0)
-					break;
-					
-				}
-			channel.deleteMessages(messageList).queue();
-			}
+			borrarMensajesBot(event);
+			
+		} else if (content.equals(comando3)) {
+			borrarMensajes(event);
+			
+		} else if(content.equals(comando4)) {
+			
 		}
-
+		
 	}
+	
+
+	private void borrarMensajes(MessageReceivedEvent event) {
+		TextChannel channel = event.getTextChannel();
+		int i = 10;
+		List<Message> messageList = new ArrayList();
+		for( Message message3 : channel.getIterableHistory()) {
+			messageList.add(message3);
+			if (i-- < 0)
+				break;	
+			}		
+		channel.deleteMessages(messageList).queue();
+	}
+
+	private void borrarMensajesBot(MessageReceivedEvent event) {
+		TextChannel channel = event.getTextChannel();
+		int i = 10;
+		List <Message> messageList = new ArrayList();
+		for (Message message2 : channel.getIterableHistory()) {// substring lee desde la posicion 0, 1 carácter
+			if (message2.getAuthor().getId().equals(botClientID) || message2.getContentRaw().substring(0, tamPrefijo).equals(prefijo)) {
+		         messageList.add(message2);
+			} 
+
+			if (i-- < 0)
+				break;
+				
+			}
+		channel.deleteMessages(messageList).queue();
+	} 
+
+}
